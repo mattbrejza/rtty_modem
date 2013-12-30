a03a51b5c1d98d3250703819f1856ee7d0e6d317
Matthew Brejza <mbrejza@gmail.com>
2013-12-30 15:28:07 +0000

    cleaning up

--- matlab/ecc/temp4/combine.m
+++
@@ -1,9 +0,0 @@
-function ber = combine(ber,input,pos)
-
-start_p = find(ber(:,1)>=input(1,1));
-end_p = start_p + size(input,1)-1;
-
-ber(start_p:end_p,pos+1) = input(:,end-2)./ input(:,2);
-%ber(start_p:end_p,pos+1) = input(:,end)./ input(:,end-1);
-
-end
